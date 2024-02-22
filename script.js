9528a9e51d7c50b36250f8f5bcf0f1137dd16bd0
nickpedro <pedrosalomaoventura1516@gmail.com>
2024-02-22 17:16:39 -0300

    Atualização 220224

---
+++ script.js
@@ -0,0 +1,6 @@
+const navbar = document.querySelector(".navbar");
+const menuButton = document.querySelector(".menu-button");
+
+menuButton.addEventListener('click', () => {
+    navbar.classList.toggle("show-menu");
+})
